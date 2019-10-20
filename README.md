288c89c09da3f909118e21e1eba244f05948e41e
krzysztof.ebert <krzysztof.ebert@gmail.com>
2019-10-20 20:25:29 +0200

    Release data

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# py-avataaars
+# py-avataaars - Python component for Avataaars
+
+Python port of https://github.com/fangpenlin/avataaars
+
+## Features
+* SVG based
+* Light weight
+* Easy to use
+
+## Usage
+
+Basic usage:
+
+```python
+from py_avataaars import PyAvataaar
+
+avatar = PyAvataaar()
+avatar.render_png_file('<output_file.png>')
+```
+
+Specify each part of avatar:
+```python
+import py_avataaars as pa
+avatar = pa.PyAvataaar(
+    style=pa.AvatarStyle.CIRCLE,
+    skin_color=pa.SkinColor.LIGHT,
+    hair_color=pa.HairColor.BROWN,
+    facial_hair_type=pa.FacialHairType.DEFAULT,
+    facial_hair_color=pa.FacialHairColor.BLACK,
+    top_type=pa.TopType.SHORT_HAIR_SHORT_FLAT,
+    hat_color=pa.ClotheColor.BLACK,
+    mouth_type=pa.MouthType.SMILE,
+    eye_type=pa.EyesType.DEFAULT,
+    eyebrow_type=pa.EyebrowType.DEFAULT,
+    accessories_type=pa.AccessoriesType.DEFAULT,
+    clothe_type=pa.ClotheType.GRAPHIC_SHIRT,
+    clothe_color=pa.ClotheColor.HEATHER,
+    clothe_graphic_type=pa.ClotheGraphicType.BAT,
+)
+avatar.render_png_file('<output_file.png>')
+```
---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='py-avataaars',
+    version='0.1.1',
+    license='MIT',
+    description='Python Avatar generator library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Krzysztof Ebert',
+    author_email='krzysztof.ebert@gmail.com',
+    url='https://github.com/kebu/py-avataaars',
+    keywords=['Python', 'Avatar', 'Avataaars', 'SVG', 'PNG', 'Generator'],
+    python_requires=">=3.6",
+    install_requires=[
+        "cairosvg >= 2.3.0",
+        'jinja2 >= 2.9.3',
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',  # Again, pick a license
+        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+        'Programming Language :: Python :: 3.6',
+    ],
+)
